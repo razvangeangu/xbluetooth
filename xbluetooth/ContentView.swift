197d6298d2016ee6ead8c9b7084774485806dbba
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2023-08-03 13:56:39 +0100

    chore: init

---
+++ xbluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xbluetooth
+//
+//  Created by Razvan-Gabriel Geangu on 03/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xbluetooth/xbluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  xbluetoothApp.swift
+//  xbluetooth
+//
+//  Created by Razvan-Gabriel Geangu on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct xbluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
